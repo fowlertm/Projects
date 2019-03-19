75700376805959f1eff5fefcedaf241061140fab
Trent Fowler <fowlertm@hendrix.edu>
2019-03-19 11:34:18 -0600

    Added a concepts file

---
+++ concepts.py
@@ -0,0 +1,14 @@
+import numpy as np 
+concepts = [
+    'one-hot encoding',
+    'categorical variables',
+    'features should be available at prediction time',
+    'the self parameter', 
+    'class methods',
+    'decorators',
+    'recursion',
+    'ML v.s. stats',
+    'OOP',
+    'instance methods',
+    
+]
